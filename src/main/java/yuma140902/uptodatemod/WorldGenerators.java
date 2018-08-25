d274b929ca0cf33384feb3337747fca3baa8bca9
yuma140902 <yuma140902@gmail.com>
2018-08-26 07:46:15 +0900

    花崗岩、閃緑岩、安山岩がワールドに生成されるようにした

---
+++ src/main/java/yuma140902/uptodatemod/WorldGenerators.java
@@ -0,0 +1,16 @@
+package yuma140902.uptodatemod;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import yuma140902.uptodatemod.worldgen.MyMinableGenerator;
+
+public final class WorldGenerators {
+	private WorldGenerators() {}
+	
+	private static final int WORLD_GENERATOR_PRIORITY = 0; //小さいほど優先度が高い
+	
+	public static void register() {
+		GameRegistry.registerWorldGenerator(myMinableGenerator, WORLD_GENERATOR_PRIORITY);
+	}
+	
+	public static final MyMinableGenerator myMinableGenerator = new MyMinableGenerator(); 
+}
