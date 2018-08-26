709ee0755aa6fe3e9374d9737b37fe0157c40f0c
yuma140902 <yuma140902@gmail.com>
2018-08-26 21:52:40 +0900

    レシピ登録部分のリファクタリング

--- src/main/java/yuma140902/uptodatemod/Recipes.java
+++ src/main/java/yuma140902/uptodatemod/Recipes.java
@@ -10,46 +10,50 @@ public final class Recipes {
 	private Recipes() {}
 	
 	public static void register() {
-		//4つ並べて磨かれた〇〇
-		GameRegistry.addRecipe(
-				new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_GRANITE), 
-				"##",
-				"##",
-				'#', new ItemStack(MyBlocks.stone, 1, Stone.META_GRANITE));
-		
-		GameRegistry.addRecipe(
-				new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_DIORITE), 
-				"##",
-				"##",
-				'#', new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE));
-		
-		GameRegistry.addRecipe(
-				new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_ANDESITE), 
-				"##",
-				"##",
-				'#', new ItemStack(MyBlocks.stone, 1, Stone.META_ANDESITE));
-		
-		//丸石+ネザー水晶->閃緑岩
-		GameRegistry.addRecipe(
-				new ItemStack(MyBlocks.stone, 2, Stone.META_DIORITE),
-				"SN",
-				"NS",
-				'S', Blocks.cobblestone,
-				'N', Items.quartz
-				);
-		
-		//閃緑岩+丸石->安山岩
-		GameRegistry.addShapelessRecipe(
-				new ItemStack(MyBlocks.stone, 2, Stone.META_ANDESITE),
-				Blocks.cobblestone,
-				new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE)
-				);
-		
-		//閃緑岩+ネザー水晶->花崗岩
-		GameRegistry.addShapelessRecipe(
-				new ItemStack(MyBlocks.stone, 2, Stone.META_GRANITE),
-				Items.quartz,
-				new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE)
-				);
+		registerStoneRecipes();
+	}
+	
+	private static void registerStoneRecipes() {
+	//4つ並べて磨かれた〇〇
+			GameRegistry.addRecipe(
+					new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_GRANITE), 
+					"##",
+					"##",
+					'#', new ItemStack(MyBlocks.stone, 1, Stone.META_GRANITE));
+			
+			GameRegistry.addRecipe(
+					new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_DIORITE), 
+					"##",
+					"##",
+					'#', new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE));
+			
+			GameRegistry.addRecipe(
+					new ItemStack(MyBlocks.stone, 4, Stone.META_SMOOTH_ANDESITE), 
+					"##",
+					"##",
+					'#', new ItemStack(MyBlocks.stone, 1, Stone.META_ANDESITE));
+			
+			//丸石+ネザー水晶->閃緑岩
+			GameRegistry.addRecipe(
+					new ItemStack(MyBlocks.stone, 2, Stone.META_DIORITE),
+					"SN",
+					"NS",
+					'S', Blocks.cobblestone,
+					'N', Items.quartz
+					);
+			
+			//閃緑岩+丸石->安山岩
+			GameRegistry.addShapelessRecipe(
+					new ItemStack(MyBlocks.stone, 2, Stone.META_ANDESITE),
+					Blocks.cobblestone,
+					new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE)
+					);
+			
+			//閃緑岩+ネザー水晶->花崗岩
+			GameRegistry.addShapelessRecipe(
+					new ItemStack(MyBlocks.stone, 2, Stone.META_GRANITE),
+					Items.quartz,
+					new ItemStack(MyBlocks.stone, 1, Stone.META_DIORITE)
+					);
 	}
 }
