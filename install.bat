5a5ddd1ef1ad09e23dc4fae262dbfd0d6b695a5f
yuma140902 <yuma140902@gmail.com>
2018-09-08 07:06:22 +0900

    build.gradleにライブラリ(deobf版の他modのjarなど)の読み込み処理を追加した
    ここhttps://defeatedcrow.jp/modwiki/%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E3%82%92%E4%BD%9C%E3%82%8B1%E3%83%BB%E5%9F%BA%E6%9C%AC%E7%92%B0%E5%A2%83%E7%94%9F%E6%88%90%E7%B7%A8
    を参考にした。

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+gradlew setupDevWorkspace setupDecompWorkspace eclipse
+pause
+
+rem https://defeatedcrow.jp/modwiki/%E9%96%8B%E7%99%BA%E7%92%B0%E5%A2%83%E3%82%92%E4%BD%9C%E3%82%8B1%E3%83%BB%E5%9F%BA%E6%9C%AC%E7%92%B0%E5%A2%83%E7%94%9F%E6%88%90%E7%B7%A8
