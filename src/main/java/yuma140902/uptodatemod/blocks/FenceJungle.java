69d2b5764a29954c45525bccd228523187a69a1e
yuma140902 <yuma140902@gmail.com>
2018-09-08 11:46:40 +0900

    フェンスゲートとこのmodで追加されるフェンスがくっつくようにした
    異なる種類のフェンスがくっつかないのは仕様。
    バニラのフェンスとこのmodで追加されるフェンスゲートがくっつかないのはバグ。

--- src/main/java/yuma140902/uptodatemod/blocks/FenceJungle.java
+++ src/main/java/yuma140902/uptodatemod/blocks/FenceJungle.java
@@ -1,10 +1,14 @@
 package yuma140902.uptodatemod.blocks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockFence;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.IBlockAccess;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
+import yuma140902.uptodatemod.MyBlocks;
 
 public class FenceJungle extends BlockFence implements IRegisterable {
 	public FenceJungle() {
@@ -20,4 +24,28 @@ public class FenceJungle extends BlockFence implements IRegisterable {
 		this.setBlockTextureName(ModUpToDateMod.MOD_ID + ":fence_jungle");
 		GameRegistry.registerBlock(this, "fence_jungle");
 	}
+	
+	/**
+   * Returns true if the specified block can be connected by a fence
+   */
+	@Override
+  public boolean canConnectFenceTo(IBlockAccess p_149826_1_, int p_149826_2_, int p_149826_3_, int p_149826_4_)
+  {
+    Block block = p_149826_1_.getBlock(p_149826_2_, p_149826_3_, p_149826_4_);
+    if(block.getMaterial().isOpaque() && block.renderAsNormalBlock()) {
+    	return block.getMaterial() != Material.gourd;
+    }
+    else if(block == this || block == Blocks.fence_gate
+    		|| block == MyBlocks.fenceGateAcacia
+    		|| block == MyBlocks.fenceGateBirch
+    		|| block == MyBlocks.fenceGateDarkOak
+    		|| block == MyBlocks.fenceGateJungle
+    		|| block == MyBlocks.fenceGateSpruce
+    		) {
+    	return true;
+    }
+    else {
+    	return false;
+    }
+  }
 }
