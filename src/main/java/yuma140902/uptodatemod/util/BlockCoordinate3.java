91449df849a330ec546e462393212eabcbad539e
yuma140902 <yuma140902@gmail.com>
2018-10-14 07:06:59 +0900

    ブロックの座標を表すクラスを追加。ユーティリティ用

---
+++ src/main/java/yuma140902/uptodatemod/util/BlockCoordinate3.java
@@ -0,0 +1,14 @@
+package yuma140902.uptodatemod.util;
+
+public class BlockCoordinate3 {
+	
+	public BlockCoordinate3(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public int x;
+	public int y;
+	public int z;
+}
