013fdb06bc5c77636b1884298264fa9e0fa120c1
yuma140902 <yuma140902@gmail.com>
2018-10-14 06:53:45 +0900

    privateをpublicに変更

--- src/main/java/yuma140902/uptodatemod/util/DirectionUtil.java
+++ src/main/java/yuma140902/uptodatemod/util/DirectionUtil.java
@@ -3,7 +3,7 @@ package yuma140902.uptodatemod.util;
 public class DirectionUtil {
 	private DirectionUtil() {}
 	
-	private static int getBack(int side) {
+	public static int getBack(int side) {
 		switch (side) {
 			case Stat.SIDE_NORTH: return Stat.SIDE_SOUTH;
 			case Stat.SIDE_SOUTH: return Stat.SIDE_NORTH;
@@ -15,7 +15,7 @@ public class DirectionUtil {
 		}
 	}
 	
-	private static int getRightSide(int side) {
+	public static int getRightSide(int side) {
 		switch (side) {
 			case Stat.SIDE_NORTH: return Stat.SIDE_EAST;
 			case Stat.SIDE_EAST: return Stat.SIDE_SOUTH;
@@ -25,7 +25,7 @@ public class DirectionUtil {
 		}
 	}
 	
-	private static int getLeftSide(int side) {
+	public static int getLeftSide(int side) {
 		switch (side) {
 			case Stat.SIDE_NORTH: return Stat.SIDE_WEST;
 			case Stat.SIDE_WEST: return Stat.SIDE_SOUTH;
