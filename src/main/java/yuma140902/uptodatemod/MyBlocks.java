91bcab48e9c57ab39736e92cf1b55e0a37189b6a
yuma140902 <yuma140902@gmail.com>
2018-11-03 17:13:56 +0900

    リファクタリング: 型を保持するようにした

--- src/main/java/yuma140902/uptodatemod/MyBlocks.java
+++ src/main/java/yuma140902/uptodatemod/MyBlocks.java
@@ -1,6 +1,5 @@
 package yuma140902.uptodatemod;
 
-import net.minecraft.block.Block;
 import yuma140902.uptodatemod.blocks.BlockObserver;
 import yuma140902.uptodatemod.blocks.CoarseDirt;
 import yuma140902.uptodatemod.blocks.DoorAcacia;
@@ -8,9 +7,9 @@ import yuma140902.uptodatemod.blocks.DoorBirch;
 import yuma140902.uptodatemod.blocks.DoorDarkOak;
 import yuma140902.uptodatemod.blocks.DoorJungle;
 import yuma140902.uptodatemod.blocks.DoorSpruce;
-import yuma140902.uptodatemod.blocks.generics.GenericTrapDoor;
 import yuma140902.uptodatemod.blocks.generics.GenericFence;
 import yuma140902.uptodatemod.blocks.generics.GenericFenceGate;
+import yuma140902.uptodatemod.blocks.generics.GenericTrapDoor;
 import yuma140902.uptodatemod.blocks.ocean_monument.BlockDarkPrismarine;
 import yuma140902.uptodatemod.blocks.ocean_monument.BlockPrismarine;
 import yuma140902.uptodatemod.blocks.ocean_monument.BlockPrismarineBrick;
@@ -21,24 +20,24 @@ public final class MyBlocks {
 	private MyBlocks() {}
 	
 	public static void register() {
-		((IRegisterable) stone).register();
-		((IRegisterable) doorAcacia).register();
-		((IRegisterable) doorBirch).register();
-		((IRegisterable) doorDarkOak).register();
-		((IRegisterable) doorJungle).register();
-		((IRegisterable) doorSpruce).register();
-		((IRegisterable) fenceAcacia).register();
-		((IRegisterable) fenceBirch).register();
-		((IRegisterable) fenceDarkOak).register();
-		((IRegisterable) fenceJungle).register();
-		((IRegisterable) fenceSpruce).register();
-		((IRegisterable) fenceGateAcacia).register();
-		((IRegisterable) fenceGateBirch).register();
-		((IRegisterable) fenceGateDarkOak).register();
-		((IRegisterable) fenceGateJungle).register();
-		((IRegisterable) fenceGateSpruce).register();
+		stone.register();
+		doorAcacia.register();
+		doorBirch.register();
+		doorDarkOak.register();
+		doorJungle.register();
+		doorSpruce.register();
+		fenceAcacia.register();
+		fenceBirch.register();
+		fenceDarkOak.register();
+		fenceJungle.register();
+		fenceSpruce.register();
+		fenceGateAcacia.register();
+		fenceGateBirch.register();
+		fenceGateDarkOak.register();
+		fenceGateJungle.register();
+		fenceGateSpruce.register();
 		
-		((IRegisterable) coarseDirt).register();
+		coarseDirt.register();
 		
 		prismarineBlock.register();
 		prismarineBrick.register();
@@ -54,26 +53,26 @@ public final class MyBlocks {
 		trapDoorSpruce.register();
 	}
 	
-	public static final Block stone = new yuma140902.uptodatemod.blocks.Stone();
-	public static final Block doorAcacia = new DoorAcacia();
-	public static final Block doorBirch = new DoorBirch();
-	public static final Block doorDarkOak = new DoorDarkOak();
-	public static final Block doorJungle = new DoorJungle();
-	public static final Block doorSpruce = new DoorSpruce();
+	public static final yuma140902.uptodatemod.blocks.Stone stone = new yuma140902.uptodatemod.blocks.Stone();
+	public static final DoorAcacia doorAcacia = new DoorAcacia();
+	public static final DoorBirch doorBirch = new DoorBirch();
+	public static final DoorDarkOak doorDarkOak = new DoorDarkOak();
+	public static final DoorJungle doorJungle = new DoorJungle();
+	public static final DoorSpruce doorSpruce = new DoorSpruce();
 	
-	public static final Block fenceAcacia = new GenericFence("planks_acacia", "fence_acacia");
-	public static final Block fenceBirch = new GenericFence("planks_birch", "fence_birch");
-	public static final Block fenceDarkOak = new GenericFence("planks_big_oak", "fence_dark_oak");
-	public static final Block fenceJungle = new GenericFence("planks_jungle", "fence_jungle");
-	public static final Block fenceSpruce = new GenericFence("planks_spruce", "fence_spruce");
+	public static final GenericFence fenceAcacia = new GenericFence("planks_acacia", "fence_acacia");
+	public static final GenericFence fenceBirch = new GenericFence("planks_birch", "fence_birch");
+	public static final GenericFence fenceDarkOak = new GenericFence("planks_big_oak", "fence_dark_oak");
+	public static final GenericFence fenceJungle = new GenericFence("planks_jungle", "fence_jungle");
+	public static final GenericFence fenceSpruce = new GenericFence("planks_spruce", "fence_spruce");
 	
-	public static final Block fenceGateAcacia = new GenericFenceGate(Stat.PLANK_META_ACACIA, "fence_gate_acacia");
-	public static final Block fenceGateBirch = new GenericFenceGate(Stat.PLANK_META_BIRCH, "fence_gate_birch");
-	public static final Block fenceGateDarkOak = new GenericFenceGate(Stat.PLANK_META_DARKOAK, "fence_gate_dark_oak");
-	public static final Block fenceGateJungle = new GenericFenceGate(Stat.PLANK_META_JUNGLE, "fence_gate_jungle");
-	public static final Block fenceGateSpruce = new GenericFenceGate(Stat.PLANK_META_SPRUCE, "fence_gate_spruce");
+	public static final GenericFenceGate fenceGateAcacia = new GenericFenceGate(Stat.PLANK_META_ACACIA, "fence_gate_acacia");
+	public static final GenericFenceGate fenceGateBirch = new GenericFenceGate(Stat.PLANK_META_BIRCH, "fence_gate_birch");
+	public static final GenericFenceGate fenceGateDarkOak = new GenericFenceGate(Stat.PLANK_META_DARKOAK, "fence_gate_dark_oak");
+	public static final GenericFenceGate fenceGateJungle = new GenericFenceGate(Stat.PLANK_META_JUNGLE, "fence_gate_jungle");
+	public static final GenericFenceGate fenceGateSpruce = new GenericFenceGate(Stat.PLANK_META_SPRUCE, "fence_gate_spruce");
 	
-	public static final Block coarseDirt = new CoarseDirt();
+	public static final CoarseDirt coarseDirt = new CoarseDirt();
 	
 	public static final BlockPrismarine prismarineBlock = new BlockPrismarine();
 	public static final BlockPrismarineBrick prismarineBrick = new BlockPrismarineBrick();
