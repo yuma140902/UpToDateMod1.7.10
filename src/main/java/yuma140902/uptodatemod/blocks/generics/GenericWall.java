7fb8fc42aa67f5b94634769dd9d9a19dedb125ea
yuma140902 <yuma140902@gmail.com>
2018-11-11 08:54:10 +0900

    メタデータを指定できるようにした

--- src/main/java/yuma140902/uptodatemod/blocks/generics/GenericWall.java
+++ src/main/java/yuma140902/uptodatemod/blocks/generics/GenericWall.java
@@ -4,23 +4,32 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
 import net.minecraft.util.IIcon;
+import net.minecraftforge.oredict.OreDictionary;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
 
 public class GenericWall extends BlockWall implements IRegisterable {
 
+	public static final int METADATA_NEUTRAL = OreDictionary.WILDCARD_VALUE;
+	
 	private Block block;
+	private int meta;
 	private String name;
 	
 	public GenericWall(Block block, String name) {
+		this(block, METADATA_NEUTRAL, name);
+	}
+	
+	public GenericWall(Block block, int meta, String name) {
 		super(block);
 		this.block = block;
+		this.meta = meta;
 		this.name = name;
 	}
 	
 	@Override
 	public IIcon getIcon(int side, int meta) {
-		return block.getIcon(side, meta);
+		return this.meta == METADATA_NEUTRAL ? block.getIcon(side, meta) : block.getIcon(side, this.meta);
 	}
 
 	@Override
