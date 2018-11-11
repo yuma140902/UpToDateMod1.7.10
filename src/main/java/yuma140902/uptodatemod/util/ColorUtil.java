8f62e328946e405e2094441e1733ad9fde2b7c9f
yuma140902 <yuma140902@gmail.com>
2018-11-11 12:43:22 +0900

    メタデータから色を得る関数

--- src/main/java/yuma140902/uptodatemod/util/ColorUtil.java
+++ src/main/java/yuma140902/uptodatemod/util/ColorUtil.java
@@ -1,6 +1,6 @@
 package yuma140902.uptodatemod.util;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
+import net.minecraft.block.material.MapColor;
 
 public class ColorUtil {
 	private ColorUtil() {}
@@ -41,6 +41,45 @@ public class ColorUtil {
 				return "yellow";
 				
 			default: return "unknown_color";
+		}
+	}
+	
+	public static MapColor metaToMapColor(int meta) {
+		switch(meta) {
+			case Stat.COLOR_META_BLACK:
+				return MapColor.blackColor;
+			case Stat.COLOR_META_BLUE:
+				return MapColor.blueColor;
+			case Stat.COLOR_META_BROWN:
+				return MapColor.brownColor;
+			case Stat.COLOR_META_CYAN:
+				return MapColor.cyanColor;
+			case Stat.COLOR_META_GRAY:
+				return MapColor.grayColor;
+			case Stat.COLOR_META_GREEN:
+				return MapColor.greenColor;
+			case Stat.COLOR_META_LIGHT_BLUE:
+				return MapColor.lightBlueColor;
+			case Stat.COLOR_META_LIME:
+				return MapColor.limeColor;
+			case Stat.COLOR_META_MAGENTA:
+				return MapColor.magentaColor;
+			case Stat.COLOR_META_ORANGE:
+				return MapColor.adobeColor;
+			case Stat.COLOR_META_PINK:
+				return MapColor.pinkColor;
+			case Stat.COLOR_META_PURPLE:
+				return MapColor.purpleColor;
+			case Stat.COLOR_META_RED:
+				return MapColor.redColor;
+			case Stat.COLOR_META_SILVER:
+				return MapColor.ironColor;
+			case Stat.COLOR_META_WHITE:
+				return MapColor.snowColor;
+			case Stat.COLOR_META_YELLOW:
+				return MapColor.yellowColor;
+				
+			default: return MapColor.airColor;
 		}
 	}
 }
