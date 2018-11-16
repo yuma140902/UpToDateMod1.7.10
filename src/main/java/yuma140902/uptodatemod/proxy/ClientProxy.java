dd8b65300339cc4d51b83b34bd690c0c597caa7e
yuma140902 <yuma140902@gmail.com>
2018-11-17 07:17:49 +0900

    プロキシクラスをメインクラスに登録

--- src/main/java/yuma140902/uptodatemod/proxy/ClientProxy.java
+++ src/main/java/yuma140902/uptodatemod/proxy/ClientProxy.java
@@ -1,5 +1,8 @@
 package yuma140902.uptodatemod.proxy;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
-	
 }
--- src/main/java/yuma140902/uptodatemod/proxy/ServerProxy.java
+++ src/main/java/yuma140902/uptodatemod/proxy/ServerProxy.java
@@ -1,5 +1,8 @@
 package yuma140902.uptodatemod.proxy;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.SERVER)
 public class ServerProxy extends CommonProxy {
-	
 }
