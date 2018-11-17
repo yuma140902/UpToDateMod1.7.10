e57824a97174b2dc989419d2ce96e40c82019256
yuma140902 <yuma140902@gmail.com>
2018-11-17 09:20:46 +0900

    ワールド読み込み時にアップデートの通知を行うようにした

---
+++ src/main/java/yuma140902/uptodatemod/event_handlers/CommonEventHandler.java
@@ -0,0 +1,7 @@
+package yuma140902.uptodatemod.event_handlers;
+
+public class CommonEventHandler {
+	private CommonEventHandler() {}
+	
+	public static final CommonEventHandler INSTANCE = new CommonEventHandler();
+}
