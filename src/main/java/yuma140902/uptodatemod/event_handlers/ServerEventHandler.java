e20431535529bdfae44c6150ad0d6dd4b253f9b1
yuma140902 <yuma140902@gmail.com>
2018-11-28 17:52:28 +0900

    サーバーのイベントハンドラを追加

---
+++ src/main/java/yuma140902/uptodatemod/event_handlers/ServerEventHandler.java
@@ -0,0 +1,7 @@
+package yuma140902.uptodatemod.event_handlers;
+
+public class ServerEventHandler {
+	private ServerEventHandler() {}
+	
+	public static final ServerEventHandler INSTANCE = new ServerEventHandler();
+}
--- src/main/java/yuma140902/uptodatemod/proxy/ServerProxy.java
+++ src/main/java/yuma140902/uptodatemod/proxy/ServerProxy.java
@@ -2,7 +2,14 @@ package yuma140902.uptodatemod.proxy;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraftforge.common.MinecraftForge;
+import yuma140902.uptodatemod.event_handlers.ServerEventHandler;
 
 @SideOnly(Side.SERVER)
 public class ServerProxy extends CommonProxy {
+	@Override
+	public void registerEventHandlers() {
+		super.registerEventHandlers();
+		MinecraftForge.EVENT_BUS.register(ServerEventHandler.INSTANCE);
+	}
 }
