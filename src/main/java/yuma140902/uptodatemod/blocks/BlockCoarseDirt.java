01e3031d57784637d625f425b7e7325825e5e6bf
yuma140902 <yuma140902@gmail.com>
2018-11-28 18:07:41 +0900

    イベントの実装
    クワで粗い土を右クリックで土にできるように

--- src/main/java/yuma140902/uptodatemod/blocks/BlockCoarseDirt.java
+++ src/main/java/yuma140902/uptodatemod/blocks/BlockCoarseDirt.java
@@ -1,5 +1,6 @@
 package yuma140902.uptodatemod.blocks;
 
+import cpw.mods.fml.common.eventhandler.Event.Result;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -9,11 +10,22 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
+import yuma140902.uptodatemod.MyBlocks;
 
 public class BlockCoarseDirt extends Block implements IRegisterable {
 
+	public static void onUseHoeEvent(UseHoeEvent event) {
+		if(event.world.getBlock(event.x, event.y, event.z) == MyBlocks.coarseDirt) {
+			event.world.setBlock(event.x, event.y, event.z, Blocks.dirt);
+			event.world.playSoundEffect(event.x + 0.5F, event.y + 0.5F, event.z + 0.5F, Block.soundTypeGravel.getStepResourcePath(), 1.0F, 0.8F);
+			event.setResult(Result.ALLOW);
+		}
+	}
+	
+	
 	public BlockCoarseDirt() {
 		super(Material.ground);
 		setHardness(0.5F);
--- src/main/java/yuma140902/uptodatemod/event_handlers/ServerEventHandler.java
+++ src/main/java/yuma140902/uptodatemod/event_handlers/ServerEventHandler.java
@@ -1,7 +1,16 @@
 package yuma140902.uptodatemod.event_handlers;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.event.entity.player.UseHoeEvent;
+import yuma140902.uptodatemod.blocks.BlockCoarseDirt;
+
 public class ServerEventHandler {
 	private ServerEventHandler() {}
 	
 	public static final ServerEventHandler INSTANCE = new ServerEventHandler();
+	
+	@SubscribeEvent
+	public void onUseHoeEvent(UseHoeEvent event) {
+		BlockCoarseDirt.onUseHoeEvent(event);
+	}
 }
