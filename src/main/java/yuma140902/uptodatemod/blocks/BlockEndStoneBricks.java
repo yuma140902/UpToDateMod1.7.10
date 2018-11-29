f28fa006c37ac0c6a2c386f8a04a15846f2fe933
yuma140902 <yuma140902@gmail.com>
2018-11-29 14:06:55 +0900

    エンドストーンレンガをエンダードラゴンが破壊できないようにした

--- src/main/java/yuma140902/uptodatemod/blocks/BlockEndStoneBricks.java
+++ src/main/java/yuma140902/uptodatemod/blocks/BlockEndStoneBricks.java
@@ -4,6 +4,9 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.world.IBlockAccess;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
 
@@ -22,5 +25,10 @@ public class BlockEndStoneBricks extends Block implements IRegisterable{
 		this.setBlockName(ModUpToDateMod.MOD_ID + ".end_stone_bricks");
 		this.setBlockTextureName(ModUpToDateMod.MOD_ID + ":end_stone_bricks");
 		GameRegistry.registerBlock(this, "end_stone_bricks");
+	}
+	
+	@Override
+	public boolean canEntityDestroy(IBlockAccess world, int x, int y, int z, Entity entity) {
+		return !(entity instanceof EntityDragon);
 	}
 }
