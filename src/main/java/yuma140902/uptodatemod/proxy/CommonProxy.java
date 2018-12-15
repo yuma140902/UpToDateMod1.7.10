740ee630bebcd87551e9b0d636410f2aca479041
yuma140902 <yuma140902@gmail.com>
2018-12-15 12:45:00 +0900

    落下中のコンクリートパウダーの更新範囲を拡大

--- src/main/java/yuma140902/uptodatemod/proxy/CommonProxy.java
+++ src/main/java/yuma140902/uptodatemod/proxy/CommonProxy.java
@@ -23,7 +23,7 @@ public class CommonProxy {
 		EntityRegistry.registerModEntity(EntityBoatDarkOak.class, "boat_dark_oak", id++, ModUpToDateMod.INSTANCE, 128, 5, true);
 		EntityRegistry.registerModEntity(EntityBoatJungle.class, "boat_jungle", id++, ModUpToDateMod.INSTANCE, 128, 5, true);
 		EntityRegistry.registerModEntity(EntityBoatSpruce.class, "boat_spruce", id++, ModUpToDateMod.INSTANCE, 128, 5, true);
-		EntityRegistry.registerModEntity(EntityFallingConcretePowderBlock.class, "falling_concrete_powder_block", id++, ModUpToDateMod.INSTANCE, 128, 1, true);
+		EntityRegistry.registerModEntity(EntityFallingConcretePowderBlock.class, "falling_concrete_powder_block", id++, ModUpToDateMod.INSTANCE, 512, 1, true);
 	}
 	
 	public void registerRenderers() { }
