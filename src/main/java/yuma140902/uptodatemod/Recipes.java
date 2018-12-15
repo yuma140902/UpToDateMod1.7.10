c953898ff677ab6ee4cb3acf53efa846b7cce923
yuma140902 <yuma140902@gmail.com>
2018-12-16 07:57:18 +0900

    赤い砂岩関連のレシピを追加

--- src/main/java/yuma140902/uptodatemod/Recipes.java
+++ src/main/java/yuma140902/uptodatemod/Recipes.java
@@ -138,6 +138,27 @@ public final class Recipes {
 				'O', Items.magma_cream
 				);
 		
+		GameRegistry.addRecipe(
+				new ItemStack(MyBlocks.redSandStone, 1, 0),
+				"##",
+				"##",
+				'#', new ItemStack(Blocks.sand, 1, 1)
+				);
+		
+		GameRegistry.addRecipe(
+				new ItemStack(MyBlocks.redSandStone, 4, 2),
+				"##",
+				"##",
+				'#', new ItemStack(MyBlocks.redSandStone, 1, 0)
+				);
+		
+		GameRegistry.addRecipe(
+				new ItemStack(MyBlocks.redSandStone, 1, 1),
+				"H",
+				"H",
+				'H', MyBlocks.slabRedSandstone
+				);
+		
 		GameRegistry.addShapelessRecipe(
 				new ItemStack(Items.dye, 9, 15),
 				MyBlocks.boneBlock
