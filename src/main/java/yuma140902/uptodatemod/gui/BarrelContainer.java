c18643297aafeb6cd790b1eae5a4e9ccfd97e2ca
yuma140902 <yuma140902@gmail.com>
2018-12-19 19:09:14 +0900

    樽のGUIでスロットを設定

--- src/main/java/yuma140902/uptodatemod/gui/BarrelContainer.java
+++ src/main/java/yuma140902/uptodatemod/gui/BarrelContainer.java
@@ -2,6 +2,7 @@ package yuma140902.uptodatemod.gui;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 import yuma140902.uptodatemod.tileentity.TileEntityBarrel;
 
 public class BarrelContainer extends Container {
@@ -9,7 +10,20 @@ public class BarrelContainer extends Container {
 	private TileEntityBarrel tileEntity;
   
   public BarrelContainer(EntityPlayer player, TileEntityBarrel tileEntity) {
-      this.tileEntity = tileEntity;
+  	this.tileEntity = tileEntity;
+      
+    // プレイヤーと樽のインベントリを設定
+  	for(int y = 0; y < 3; ++y) {
+  		for(int x = 0; x < 9; ++x) {
+  			this.addSlotToContainer(new Slot(tileEntity, x+(y*9), 8+(x*18), 18+(y*18)));
+  			this.addSlotToContainer(new Slot(player.inventory, x+(y*9)+9, 8+(x*18), 84+(y*18)));
+  		}
+  	}
+  	
+  	// プレイヤーのインベントリ(下の9つ)を設定
+  	for(int x = 0; x < 9; ++x) {
+  		this.addSlotToContainer(new Slot(player.inventory, x, 8+(x*18), 142));
+  	}
   }
 
 	@Override
