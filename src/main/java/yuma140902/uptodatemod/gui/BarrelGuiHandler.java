86d4e47bc8b01527fa0421895ee2a4c48f7bc15b
yuma140902 <yuma140902@gmail.com>
2018-12-19 18:40:42 +0900

    BarrelGuiHandlerでTileEntityを使用するようにした

--- src/main/java/yuma140902/uptodatemod/gui/BarrelGuiHandler.java
+++ src/main/java/yuma140902/uptodatemod/gui/BarrelGuiHandler.java
@@ -2,23 +2,33 @@ package yuma140902.uptodatemod.gui;
 
 import cpw.mods.fml.common.network.IGuiHandler;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import yuma140902.uptodatemod.blocks.BlockBarrel;
+import yuma140902.uptodatemod.tileentity.TileEntityBarrel;
 
 public class BarrelGuiHandler implements IGuiHandler {
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		if(ID == BlockBarrel.GUI_ID) {
-			return new BarrelContainer(x, y, z);
+		if(ID != BlockBarrel.GUI_ID || !world.blockExists(x, y, z)) {
+			return null;
+		}
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+		if(tileEntity instanceof TileEntityBarrel) {
+			return new BarrelContainer((TileEntityBarrel) tileEntity);
 		}
 		return null;
 	}
 
 	@Override
 	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
-		if(ID == BlockBarrel.GUI_ID) {
-			return new BarrelGuiContainer(x, y, z);
+		if(ID != BlockBarrel.GUI_ID || !world.blockExists(x, y, z)) {
+			return null;
+		}
+		TileEntity tileEntity = world.getTileEntity(x, y, z);
+		if(tileEntity instanceof TileEntityBarrel) {
+			return new BarrelGuiContainer((TileEntityBarrel) tileEntity);
 		}
 		return null;
 	}
