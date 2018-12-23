ac9a13021a8eedc85d435d13c4ae0b67dbbf1826
yuma140902 <yuma140902@gmail.com>
2018-12-24 07:07:25 +0900

    GUIの文字を改善

--- src/main/java/yuma140902/uptodatemod/gui/BarrelGuiContainer.java
+++ src/main/java/yuma140902/uptodatemod/gui/BarrelGuiContainer.java
@@ -1,8 +1,10 @@
 package yuma140902.uptodatemod.gui;
 
+import org.lwjgl.opengl.GL11;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StatCollector;
 import yuma140902.uptodatemod.ModUpToDateMod;
 import yuma140902.uptodatemod.tileentity.TileEntityBarrel;
 
@@ -13,23 +15,24 @@ public class BarrelGuiContainer extends GuiContainer {
 	public BarrelGuiContainer(EntityPlayer player, TileEntityBarrel tileEntity) {
 		super(new BarrelContainer(player, tileEntity));
 		this.tileEntity = tileEntity;
-		this.xSize = 176;
-		this.ySize = 166;
+		this.ySize = 168;
 	}
 	
 	/*GUIの文字等の描画処理*/
 	@Override
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-		this.fontRendererObj.drawString("Barrel", 12, 8, 0, false);
+		fontRendererObj.drawString(StatCollector.translateToLocal(tileEntity.getInventoryName()), 8, 6, 4210752);
+		fontRendererObj.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 1, 4210752);
 	}
 
 	/*GUIの背景の描画処理*/
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float partialTick, int mouseX, int mouseY) {
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.mc.renderEngine.bindTexture(TEXTURE);
-		int xStart = (this.width - this.xSize) / 2;
-    int yStart = (this.height - this.ySize) / 2;
-    this.drawTexturedModalRect(xStart, yStart, 0, 0, this.xSize, this.ySize);
+		int tmpX = (this.width - this.xSize) / 2;
+    int tmpY = (this.height - this.ySize) / 2;
+    this.drawTexturedModalRect(tmpX, tmpY, 0, 0, this.xSize, this.ySize);
 	}
 	
 	/*GUIが開いている時にゲームの処理を止めるかどうか。*/
