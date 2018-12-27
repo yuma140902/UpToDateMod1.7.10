e0517d21fc6cecfd4014ce5edf02bf299410f4bf
yuma140902 <yuma140902@gmail.com>
2018-12-27 10:57:36 +0900

    このMODで追加される壁が互いにくっつくようにした

--- src/main/java/yuma140902/uptodatemod/blocks/generics/BlockGenericWall.java
+++ src/main/java/yuma140902/uptodatemod/blocks/generics/BlockGenericWall.java
@@ -5,11 +5,14 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockFenceGate;
 import net.minecraft.block.BlockWall;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import yuma140902.uptodatemod.IHasRecipes;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
@@ -52,5 +55,12 @@ public class BlockGenericWall extends BlockWall implements IRegisterable, IHasRe
 				"###",
 				"###",
 				'#', new ItemStack(this.block, 1, this.meta));
+	}
+	
+	@Override
+	public boolean canConnectWallTo(IBlockAccess blockAccess, int x, int y, int z) {
+		Block block = blockAccess.getBlock(x, y, z);
+    return !(block instanceof BlockGenericWall) && !(block instanceof BlockFenceGate) ? (block.getMaterial().isOpaque() && block.renderAsNormalBlock() ? block.getMaterial() != Material.gourd : false) : true;
+
 	}
 }
