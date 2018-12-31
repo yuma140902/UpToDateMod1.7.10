7e32254166bb39898b04fb3fadb0a5768386facd
yuma140902 <yuma140902@gmail.com>
2018-12-31 15:58:24 +0900

    防具のエンティティと関連クラスを追加

---
+++ src/main/java/yuma140902/uptodatemod/client/renderer/RenderArmorStand.java
@@ -0,0 +1,22 @@
+package yuma140902.uptodatemod.client.renderer;
+
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.ResourceLocation;
+
+public class RenderArmorStand extends Render {
+
+	@Override
+	public void doRender(
+			Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	
+}
---
+++ src/main/java/yuma140902/uptodatemod/entity/item/EntityArmorStand.java
@@ -0,0 +1,37 @@
+package yuma140902.uptodatemod.entity.item;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+public class EntityArmorStand extends Entity {
+
+	public EntityArmorStand(World world) {
+		super(world);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+	public EntityArmorStand(World world, int x, int y, int z) {
+		this(world);
+		setPosition(x, y, z);
+	}
+
+	@Override
+	protected void entityInit() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound nbt) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound nbt) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+}
