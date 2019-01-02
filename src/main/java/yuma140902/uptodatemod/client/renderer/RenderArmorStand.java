6f45e8164b76bf516378cb29a268b3809875ef65
yuma140902 <yuma140902@gmail.com>
2019-01-02 11:49:53 +0900

    防具をEntityLivingBaseとしてRenderLivingEntityで描画

--- src/main/java/yuma140902/uptodatemod/client/renderer/RenderArmorStand.java
+++ src/main/java/yuma140902/uptodatemod/client/renderer/RenderArmorStand.java
@@ -1,22 +1,48 @@
 package yuma140902.uptodatemod.client.renderer;
 
-import net.minecraft.client.renderer.entity.Render;
+import org.lwjgl.opengl.GL11;
+import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
+import yuma140902.uptodatemod.ModUpToDateMod;
+import yuma140902.uptodatemod.client.model.ModelArmorStand;
+import yuma140902.uptodatemod.entity.item.EntityArmorStand;
 
-public class RenderArmorStand extends Render {
+public class RenderArmorStand extends RendererLivingEntity {
+	public RenderArmorStand() {
+		super(new ModelArmorStand(), 0.5F);
+	}
 
+	private ResourceLocation textureArmorStand = new ResourceLocation(ModUpToDateMod.MOD_ID, "textures/entity/armor_stand.png");
+	
+	public void doRender(EntityArmorStand entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		this.bindTexture(textureArmorStand);
+		GL11.glColor3f(1.0F, 1.0F, 1.0F);
+    
+    double d3 = y - (double)entity.yOffset;
+
+    if (entity.isSneaking())
+    {
+        d3 -= 0.125D;
+    }
+
+    super.doRender(entity, x, y, z, entityYaw, partialTicks);
+	}
+	
 	@Override
-	public void doRender(
-			Entity p_76986_1_, double p_76986_2_, double p_76986_4_, double p_76986_6_, float p_76986_8_, float p_76986_9_) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+	public void doRender(Entity entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		this.doRender((EntityArmorStand)entity, x, y, z, entityYaw, partialTicks);
+	}
+	
+	@Override
+	public void doRender(EntityLivingBase entity, double x, double y, double z, float entityYaw, float partialTicks) {
+		this.doRender((EntityArmorStand)entity, x, y, z, entityYaw, partialTicks);
 	}
 
 	@Override
 	protected ResourceLocation getEntityTexture(Entity p_110775_1_) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return textureArmorStand;
 	}
 	
 }
--- src/main/java/yuma140902/uptodatemod/entity/item/EntityArmorStand.java
+++ src/main/java/yuma140902/uptodatemod/entity/item/EntityArmorStand.java
@@ -1,37 +1,63 @@
 package yuma140902.uptodatemod.entity.item;
 
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
-public class EntityArmorStand extends Entity {
+public class EntityArmorStand extends EntityLivingBase {
 
 	public EntityArmorStand(World world) {
 		super(world);
-		// TODO 自動生成されたコンストラクター・スタブ
+		this.setSize(1.0F, 1.0F);
+		this.yOffset = 2.0F;
 	}
 	
 	public EntityArmorStand(World world, int x, int y, int z) {
 		this(world);
-		setPosition(x, y, z);
+		this.setPosition(x, y, z);
 	}
 
 	@Override
 	protected void entityInit() {
 		// TODO 自動生成されたメソッド・スタブ
-		
+		super.entityInit();
 	}
 
 	@Override
-	protected void readEntityFromNBT(NBTTagCompound nbt) {
+	public void readEntityFromNBT(NBTTagCompound nbt) {
 		// TODO 自動生成されたメソッド・スタブ
 		
 	}
 
 	@Override
-	protected void writeEntityToNBT(NBTTagCompound nbt) {
+	public void writeEntityToNBT(NBTTagCompound nbt) {
 		// TODO 自動生成されたメソッド・スタブ
 		
+	}
+
+	@Override
+	public ItemStack getHeldItem() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public ItemStack getEquipmentInSlot(int p_71124_1_) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void setCurrentItemOrArmor(int p_70062_1_, ItemStack p_70062_2_) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	public ItemStack[] getLastActiveItems() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 	
 }
