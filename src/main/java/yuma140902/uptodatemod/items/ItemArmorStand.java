89652bb792da6ef27ddfc5e5bc9b359157148e26
yuma140902 <yuma140902@gmail.com>
2019-01-02 12:05:15 +0900

    防具立てのアイテムで防具立てエンティティを召喚できるようにした

--- src/main/java/yuma140902/uptodatemod/items/ItemArmorStand.java
+++ src/main/java/yuma140902/uptodatemod/items/ItemArmorStand.java
@@ -2,14 +2,17 @@ package yuma140902.uptodatemod.items;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import yuma140902.uptodatemod.IHasRecipes;
 import yuma140902.uptodatemod.IRegisterable;
 import yuma140902.uptodatemod.ModUpToDateMod;
 import yuma140902.uptodatemod.MyBlocks;
+import yuma140902.uptodatemod.entity.item.EntityArmorStand;
 
 public class ItemArmorStand extends Item implements IRegisterable, IHasRecipes {
 	
@@ -44,4 +47,14 @@ public class ItemArmorStand extends Item implements IRegisterable, IHasRecipes {
 				'O', MyBlocks.slabStone
 				);
 	}
+	
+	@Override
+	public boolean onItemUse(ItemStack itemstack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+		EntityArmorStand armorStand = new EntityArmorStand(world, x, y+1, z);
+		if(!world.isRemote) {
+			world.spawnEntityInWorld(armorStand);
+		}
+		return true;
+	}
+	
 }
