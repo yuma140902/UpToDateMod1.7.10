9b26cfa2529092874432f082dc2e4b71372fd2af
yuma140902 <yuma140902@gmail.com>
2019-02-12 18:33:48 +0900

    フェンスのレシピを鉱石辞書を使って書き換え

--- src/main/java/yuma140902/uptodatemod/Recipes.java
+++ src/main/java/yuma140902/uptodatemod/Recipes.java
@@ -12,6 +12,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import yuma140902.uptodatemod.config.ModConfigCore;
 import yuma140902.uptodatemod.util.ListUtils;
 
@@ -141,52 +142,64 @@ public final class Recipes {
 	
 	private static void registerFenceRecipes() {
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(Blocks.fence, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_OAK,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(MyBlocks.fenceAcacia, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_ACACIA,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(MyBlocks.fenceBirch, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_BIRCH,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(MyBlocks.fenceDarkOak, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_DARK_OAK,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(MyBlocks.fenceJungle, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_JUNGLE,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 		GameRegistry.addRecipe(
+			new ShapedOreRecipe(
 				new ItemStack(MyBlocks.fenceSpruce, 3, 0),
 				"#|#",
 				"#|#",
 				'#', PLANK_SPRUCE,
-				'|', Items.stick
-				);
+				'|', "stickWood"
+			)
+		);
 		
 	}
 
