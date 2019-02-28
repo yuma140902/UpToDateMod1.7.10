6fad1d86147d4c4fbab6b91f1ca962f60ffc042b
yuma140902 <yuma140902@gmail.com>
2019-02-28 17:33:27 +0900

    gridにworld, rand, x, y, zを保存するようにした

--- src/main/java/yuma140902/uptodatemod/world/generation/woodland_mansion_B/WoodlandMansionGrid.java
+++ src/main/java/yuma140902/uptodatemod/world/generation/woodland_mansion_B/WoodlandMansionGrid.java
@@ -1,7 +1,42 @@
 package yuma140902.uptodatemod.world.generation.woodland_mansion_B;
 
+import java.util.Random;
+import net.minecraft.world.World;
+
 public class WoodlandMansionGrid {
 	public static final int GRID_X_MAX = 19, GRID_Z_MAX = 15;
+	
+	public WoodlandMansionGrid(World world, Random rand, int xCoord, int yCoord, int zCoord) {
+		this.world = world;
+		this.random = rand;
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.zCoord = zCoord;
+	}
+	
+	private int xCoord, yCoord, zCoord;
+	private World world;
+	private Random random;
+	
+	public int getXCoord() {
+		return xCoord;
+	}
+	
+	public int getYCoord() {
+		return yCoord;
+	}
+	
+	public int getZCoord() {
+		return zCoord;
+	}
+	
+	public World getWorld() {
+		return world;
+	}
+	
+	public Random getRand() {
+		return random;
+	}
 	
 	private IStructureComponentB[] grid = new IStructureComponentB[GRID_X_MAX * GRID_Z_MAX];
 	
