60b129bc72968125271417541a7aae6f0ab4c117
yuma140902 <yuma140902@gmail.com>
2019-03-09 12:53:49 +0900

    Update IStructureComponentB.java

--- src/main/java/yuma140902/uptodatemod/world/generation/woodland_mansion_B/IStructureComponentB.java
+++ src/main/java/yuma140902/uptodatemod/world/generation/woodland_mansion_B/IStructureComponentB.java
@@ -2,15 +2,15 @@ package yuma140902.uptodatemod.world.generation.woodland_mansion_B;
 
 public interface IStructureComponentB {
 	
-	void generate();
+	void generate(StructureRelativeCoordinateSystem relCoord);
 	
 	public static class InnerEmpty implements IStructureComponentB {
 		@Override
-		public void generate() {}
+		public void generate(StructureRelativeCoordinateSystem relCoord) {}
 	}
 	
 	public static class OuterEmpty implements IStructureComponentB {
 		@Override
-		public void generate() {}
+		public void generate(StructureRelativeCoordinateSystem relCoord) {}
 	}
 }
