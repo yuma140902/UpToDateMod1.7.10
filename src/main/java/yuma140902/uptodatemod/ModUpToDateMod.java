cde51ca236e05dd30f6ebc88e1c8fc3e52009e9d
yuma140902 <yuma140902@gmail.com>
2019-03-31 08:45:57 +0900

    Update ModUpToDateMod.java
    Forge起動中のログにMOD名が表示されるようになる

--- src/main/java/yuma140902/uptodatemod/ModUpToDateMod.java
+++ src/main/java/yuma140902/uptodatemod/ModUpToDateMod.java
@@ -21,7 +21,7 @@ import yuma140902.uptodatemod.util.Stat;
 import yuma140902.uptodatemod.util.UpdateChecker;
 import yuma140902.uptodatemod.world.generation.MyMinableGenerator;
 
-@Mod(modid = ModUpToDateMod.MOD_ID, version = ModUpToDateMod.MOD_VERSION, useMetadata = true, guiFactory = Stat.MOD_CONFIG_GUI_FACTORY)
+@Mod(modid = ModUpToDateMod.MOD_ID, name = ModUpToDateMod.MOD_NAME, version = ModUpToDateMod.MOD_VERSION, useMetadata = true, guiFactory = Stat.MOD_CONFIG_GUI_FACTORY)
 public class ModUpToDateMod {
 	@Mod.Metadata
 	public static ModMetadata modMetadata;
