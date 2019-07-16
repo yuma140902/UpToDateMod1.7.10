2e800717edeb96080af974a3be350533cb6455d1
yuma <yuma140902@gmail.com>
2019-07-16 17:20:51 +0900

    石関連のテクスチャをminecraftドメインに移動

--- src/main/java/yuma140902/uptodatemod/blocks/BlockStone.java
+++ src/main/java/yuma140902/uptodatemod/blocks/BlockStone.java
@@ -62,12 +62,12 @@ public class BlockStone extends Block implements IRegisterable, IHasRecipes {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerBlockIcons(IIconRegister register) {
-		iIcons[META_GRANITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("granite"));
-		iIcons[META_POLISHED_GRANITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("polished_granite"));
-		iIcons[META_DIORITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("diorite"));
-		iIcons[META_POLISHED_DIORITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("polished_diorite"));
-		iIcons[META_ANDESITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("andesite"));
-		iIcons[META_POLISHED_ANDESITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("polished_andesite"));
+		iIcons[META_GRANITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_granite"));
+		iIcons[META_POLISHED_GRANITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_polished_granite"));
+		iIcons[META_DIORITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_diorite"));
+		iIcons[META_POLISHED_DIORITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_polished_diorite"));
+		iIcons[META_ANDESITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_andesite"));
+		iIcons[META_POLISHED_ANDESITE] = register.registerIcon(StringUtil.getDomainedMCTextureName("stone_polished_andesite"));
 	}
 	
 	@Override
