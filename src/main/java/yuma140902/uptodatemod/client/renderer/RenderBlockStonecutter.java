74b112352f015c35304b1029d3c541b50082280f
yuma <yuma140902@gmail.com>
2019-07-24 07:07:22 +0900

    Stonecutterののこぎり部分が描画処理を追加

--- src/main/java/yuma140902/uptodatemod/client/renderer/RenderBlockStonecutter.java
+++ src/main/java/yuma140902/uptodatemod/client/renderer/RenderBlockStonecutter.java
@@ -3,6 +3,7 @@ package yuma140902.uptodatemod.client.renderer;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import yuma140902.uptodatemod.ModUpToDateMod;
 import yuma140902.uptodatemod.blocks.BlockStonecutter;
@@ -20,14 +21,30 @@ public class RenderBlockStonecutter implements ISimpleBlockRenderingHandler {
 		if(modelId != getRenderId()) return false;
 		
 		int meta = world.getBlockMetadata(x, y, z);
-		if(meta == BlockStonecutter.META_EAST || meta == BlockStonecutter.META_WEST) {
+		boolean isRotated = (meta == BlockStonecutter.META_EAST || meta == BlockStonecutter.META_WEST);
+		if(isRotated) {
 			renderer.uvRotateTop = 1;
 		}
 		
 		boolean hasRendered = renderer.renderStandardBlock(block, x, y, z);
 		
+		renderSawInWorld(world, x, y, z, isRotated, (BlockStonecutter)block, renderer);
+		
 		renderer.uvRotateTop = 0;
 		return hasRendered;
+	}
+	
+	private void renderSawInWorld(IBlockAccess world, int x, int y, int z, boolean isRotated, BlockStonecutter block, RenderBlocks renderer) {
+		if(!isRotated) block.setBlockBounds(0.0f, 9.0f/16, 0.5f, 1.0f, 25.0f/16, 0.5f);
+		else block.setBlockBounds(0.5f, 9.0f/16, 0.0f, 0.5f, 25.0f/16, 1.0f);
+		renderer.setRenderBoundsFromBlock(block);
+		
+		IIcon iconSaw = block.getIconSaw();
+		renderer.setOverrideBlockTexture(iconSaw);
+		renderer.renderStandardBlock(block, x, y, z);
+		
+		renderer.clearOverrideBlockTexture();
+		block.setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 0.5625f, 1.0f);
 	}
 
 	@Override
