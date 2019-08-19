279f9bc41066d462e1e84c2ddb52ced6c38b162e
yuma <yuma140902@gmail.com>
2019-08-20 08:36:03 +0900

    StonecutterGuiContainerにScrollBarのイベントハンドラを追加

--- src/main/java/yuma140902/uptodatemod/gui/StonecutterGuiContainer.java
+++ src/main/java/yuma140902/uptodatemod/gui/StonecutterGuiContainer.java
@@ -6,16 +6,39 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import yuma140902.uptodatemod.ModUpToDateMod;
+import yuma140902.uptodatemod.gui.event.IEventHandler;
+import yuma140902.uptodatemod.gui.util.IResourceLocationPart;
+import yuma140902.uptodatemod.gui.util.ResourceLocationPart;
+import yuma140902.uptodatemod.gui.widgets.GuiScrollBar;
+import yuma140902.uptodatemod.gui.widgets.GuiScrollBar.ScrollChangeEvent;
 import yuma140902.uptodatemod.tileentity.TileEntityStonecutter;
 
 public class StonecutterGuiContainer extends GuiContainer {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(ModUpToDateMod.MOD_TEXTURE_DOMAIN, "textures/gui/stonecutter.png");
+	private static final IResourceLocationPart SCROLLBAR_RES = new ResourceLocationPart(TEXTURE, 176, 0, 12, 15);
+	private static final int idScrollBar = 0;
+	private GuiScrollBar scrollBar;
 	private TileEntityStonecutter tileentity;
 	
 	public StonecutterGuiContainer(EntityPlayer player, TileEntityStonecutter tileentity) {
 		super(new StonecutterContainer(player, tileentity));
 		this.tileentity = tileentity;
 		this.ySize = 166;
+	}
+	
+	@Override
+	public void initGui() {
+		super.initGui();
+		this.buttonList.clear();
+		int x = (this.width - this.xSize) / 2;
+		int y = (this.height - this.ySize) / 2;
+		this.buttonList.add(this.scrollBar = new GuiScrollBar(idScrollBar, x+119, y+15, 12, 54, 5, SCROLLBAR_RES));
+		scrollBar.scrollTo(tileentity.getGuiScroll());
+		scrollBar.subscribe(new IEventHandler<GuiScrollBar.ScrollChangeEvent>() {
+			public void onFire(ScrollChangeEvent event) {
+				tileentity.setGuiScroll(event.scrollValue);
+			}
+		});
 	}
 	
 	@Override
