15f2956d58b26b43837fb57bd7c3e285b76c8495
yuma <yuma140902@gmail.com>
2019-08-20 15:45:06 +0900

    Shift+クリックでアイテムを移動できるようにした

--- src/main/java/yuma140902/uptodatemod/gui/StonecutterContainer.java
+++ src/main/java/yuma140902/uptodatemod/gui/StonecutterContainer.java
@@ -4,12 +4,29 @@ import invtweaks.api.container.ChestContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import yuma140902.uptodatemod.tileentity.TileEntityStonecutter;
 
 @ChestContainer
 public class StonecutterContainer extends Container {
 	
 	private TileEntityStonecutter tileentity;
+	
+	/*
+	 * スロット番号の配置
+	 *  0                       1
+	 * --------------------------
+	 * 11 12 13 14 15 16 17 18 19
+	 * 20 21 22 23 24 25 26 27 28
+	 * 29 30 31 32 33 34 35 36 37
+	 * --------------------------
+	 *  2  3  4  5  6  7  8  9 10
+	 */
+	private static final int
+		stonecutterIndexStart = 0,
+		stonecutterIndexEnd = 1,
+		hotbarIndexStart = 2,
+		playerIndexEnd = 37;
 	
 	public StonecutterContainer(EntityPlayer player, TileEntityStonecutter tileentity) {
 		this.tileentity = tileentity;
@@ -34,5 +51,37 @@ public class StonecutterContainer extends Container {
 	@Override
 	public boolean canInteractWith(EntityPlayer player) {
 		return tileentity.isUseableByPlayer(player);
+	}
+	
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int slotNo) {
+		Slot slot = (Slot) this.inventorySlots.get(slotNo);
+		
+		if(slot == null || !slot.getHasStack()) {
+			return null;
+		}
+		
+		ItemStack itemstack = slot.getStack();
+		ItemStack itemstackBak = itemstack.copy();
+		
+		if(slotNo <= stonecutterIndexEnd) {
+			if(!mergeItemStack(itemstack, hotbarIndexStart, playerIndexEnd+1, true)) {
+				return null;
+			}
+		}
+		else {
+			if(!mergeItemStack(itemstack, stonecutterIndexStart, stonecutterIndexStart+1, false)) {
+				return null;
+			}
+		}
+		
+		if (itemstack.stackSize == 0) {
+			slot.putStack((ItemStack) null);
+		}
+		else {
+			slot.onSlotChanged();
+		}
+		
+		return itemstackBak;
 	}
 }
