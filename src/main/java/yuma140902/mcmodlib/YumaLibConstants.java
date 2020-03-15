29eae6dbf41ff424aca032ae0a91e01d9a16fe19
yuma <yuma140902@gmail.com>
2020-03-15 14:54:39 +0900

    YumaLibをYumaLib Embedded Editionにした

--- src/main/java/yuma140902/mcmodlib/YumaLibConstants.java
+++ src/main/java/yuma140902/mcmodlib/YumaLibConstants.java
@@ -1,8 +1,8 @@
 package yuma140902.mcmodlib;
 
 public class YumaLibConstants {
-	public static final String MOD_NAME = "YumaLib 0";
-	public static final String MOD_ID = "yumalib0";
+	public static final String MOD_NAME = "YumaLib Embedded Edition";
+	public static final String MOD_ID = "yumalib_ee";
 	public static final String MOD_VERSION = "0.0.0";
 	public static final String MC_VERSION = "1.7.10";
 	
