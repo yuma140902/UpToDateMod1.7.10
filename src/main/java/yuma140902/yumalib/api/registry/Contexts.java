61782d1fd64bf5a87ddd6518a3c720e372c35eab
yuma <yuma140902@gmail.com>
2020-04-02 17:40:05 +0900

    InitModContextにaddonModNameを追加
    ドキュメントの更新

--- src/main/java/yuma140902/yumalib/api/registry/Contexts.java
+++ src/main/java/yuma140902/yumalib/api/registry/Contexts.java
@@ -2,20 +2,20 @@ package yuma140902.yumalib.api.registry;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import yuma140902.yumalib.ModYumaLib;
 import yuma140902.yumalib.YLConstants;
 import yuma140902.yumalib.api.IRegisterable;
 import yuma140902.yumalib.api.context.InitModContext;
 import yuma140902.yumalib.api.util.NameProvider;
 
 /**
- * Mod初期化時の文脈(YumaLibのAPIを呼び出しているのはどのModかなどの情報)を表す。
- * これにより、いちいちメソッドにMod名などを渡す手間が省ける
+ * {@link InitModContext}のレジストリ
  * @author yuma1
  *
  */
 public class Contexts {
 	@Nonnull
-	public static final InitModContext DEFAULT = new InitModContext(new NameProvider(YLConstants.MOD_ID, YLConstants.MOD_ID));
+	public static final InitModContext DEFAULT = new InitModContext(YLConstants.MOD_NAME, new NameProvider(YLConstants.MOD_ID, YLConstants.MOD_ID));
 	@Nullable
 	private static InitModContext current = null;
 	
@@ -28,11 +28,13 @@ public class Contexts {
 		return current != null ? current : DEFAULT;
 	}
 	
-	public static void setContext(InitModContext context) {
+	public static void setContext(@Nonnull InitModContext context) {
 		current = context;
+		ModYumaLib.LOGGER.info("Context updated: " + context.addonModName());
 	}
 	
 	public static void removeContext() {
 		current = null;
+		ModYumaLib.LOGGER.info("Context reset");
 	}
 }
