816e66f5a7972c8b2610120fc1d3b3bf8e7be3d0
yuma <yuma140902@gmail.com>
2021-02-07 09:08:45 +0900

    StripWoodHandlerでVanillaRotatedPillarStateを使うようにして、直接のビット操作をなくした

---
+++ src/main/java/yuma140902/yumalib/api/blockstate/EnumVanillaLog.java
@@ -0,0 +1,11 @@
+package yuma140902.yumalib.api.blockstate;
+
+/**
+ * minecraft:log
+ */
+public enum EnumVanillaLog {
+	Oak,
+	Spruce,
+	Birch,
+	Jungle
+}
---
+++ src/main/java/yuma140902/yumalib/api/blockstate/EnumVanillaLog2.java
@@ -0,0 +1,9 @@
+package yuma140902.yumalib.api.blockstate;
+
+/**
+ * minecraft:log2
+ */
+public enum EnumVanillaLog2 {
+	Acacia,
+	DarkOak
+}
