13f4e1ebf7c548a71e8a19d59fd03da37aa54301
yuma <yuma140902@gmail.com>
2021-02-07 11:33:53 +0900

    鉄のトラップドアを好きな方向に向かせるのを簡単にした

--- src/main/java/yuma140902/yumalib/api/blocks/BlockGenericTrapDoor.java
+++ src/main/java/yuma140902/yumalib/api/blocks/BlockGenericTrapDoor.java
@@ -8,7 +8,6 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
@@ -18,34 +17,61 @@ import yuma140902.yumalib.api.IRegisterable;
 import yuma140902.yumalib.api.McConst;
 import yuma140902.yumalib.api.items.ItemBlockGenericTrapdoor;
 import yuma140902.yumalib.api.registry.Contexts;
+import yuma140902.yumalib.api.util.BlockWithMetadata;
+import yuma140902.yumalib.api.util.ItemWithMetadata;
 import yuma140902.yumalib.api.util.WorldUtils;
 import yuma140902.yumalib.api.blockstate.GenericTrapdoorState;
 
 public class BlockGenericTrapDoor extends BlockTrapDoor implements IRegisterable, IHasRecipes {
 	/** Set this to allow trapdoors to remain free-floating */
-  private String name;
-  private int plankMeta;
+  private final String name;
+  private final ItemWithMetadata ingredient;
+  private final boolean rotateTex;
+  private final boolean sideTex;
+  private final BlockWithMetadata blockForSideTex;
   
   private IIcon icon0, icon90, icon180, icon270;
-
-  public BlockGenericTrapDoor(String name, int plankMeta)
+	
+	/**
+	 * @param material ブロックの材質
+	 * @param name ブロックの名前
+	 * @param ingredient 原料
+	 * @param rotateTex 上面・下面のテクスチャを回転させるかどうか
+	 * @param sideTex 側面のテクスチャとして別ブロックのものを使うかどうか
+	 * @param blockForSideTex 側面のテクスチャとして使うブロック
+	 */
+  protected BlockGenericTrapDoor(Material material, String name, float hardness, SoundType stepSound, ItemWithMetadata ingredient, boolean rotateTex, boolean sideTex, BlockWithMetadata blockForSideTex)
   {
-      super(Material.wood);
+      super(material);
       this.name = name;
-      this.plankMeta = plankMeta;
+      this.ingredient = ingredient;
+      this.rotateTex = rotateTex;
+      this.sideTex = sideTex;
+      this.blockForSideTex = blockForSideTex;
+      
       this.setCreativeTab(CreativeTabs.tabRedstone);
-      this.setHardness(3.0F);
-      this.setStepSound(soundTypeWood);
+      this.setHardness(hardness);
+      this.setStepSound(stepSound);
   }
   
   @Override
   public void registerBlockIcons(IIconRegister register) {
   	super.registerBlockIcons(register);
   	icon0 = register.registerIcon(getTextureName());
-  	icon90 = register.registerIcon(getTextureName() + "_90");
-  	icon180 = register.registerIcon(getTextureName() + "_180");
-  	icon270 = register.registerIcon(getTextureName() + "_270");
+  	if(this.rotateTex) {
+			icon90 = register.registerIcon(getTextureName() + "_90");
+			icon180 = register.registerIcon(getTextureName() + "_180");
+			icon270 = register.registerIcon(getTextureName() + "_270");
+		}
+  	else{
+  		icon90 = icon180 = icon270 = icon0;
+		}
   }
+  
+  @SideOnly(Side.CLIENT)
+  private IIcon getSideIcon(){
+		return sideTex ? this.blockForSideTex.block.getIcon(McConst.SIDE_TOP, this.blockForSideTex.meta) : icon0;
+	}
   
   @Override
   @SideOnly(Side.CLIENT)
@@ -69,33 +95,22 @@ public class BlockGenericTrapDoor extends BlockTrapDoor implements IRegisterable
     				return icon90;
     		}
     	}
-    	
-    	else {
-    		return Blocks.planks.getIcon(1, plankMeta);
-    	}
+    	return getSideIcon();
   	}
   	
   	else if(isTop) {
   		if(GenericTrapdoorState.Orientation.isSameSide(side, orientation) || GenericTrapdoorState.Orientation.isOppositeSide(side, orientation)) {
   			return icon180;
   		}
-  		
-  		else {
-  			return Blocks.planks.getIcon(1, plankMeta);
-  		}
+  		return getSideIcon();
   	}
   	
   	else {
   		if(GenericTrapdoorState.Orientation.isSameSide(side, orientation) || GenericTrapdoorState.Orientation.isOppositeSide(side, orientation)) {
   			return icon0;
   		}
-  		
-  		else {
-  			return Blocks.planks.getIcon(1, plankMeta);
-  		}
+  		return getSideIcon();
   	}
-  	
-  	return Blocks.planks.getIcon(1, plankMeta);
   }
   
   @Override
@@ -107,11 +122,12 @@ public class BlockGenericTrapDoor extends BlockTrapDoor implements IRegisterable
   
   @Override
   public void registerRecipes() {
+  	if(ingredient == null) return;
   	RecipeRegister.addShaped(
 				new ItemStack(this, 2, 0),
 				"###",
 				"###",
-				'#', new ItemStack(Blocks.planks, 1, plankMeta)
+				'#', this.ingredient.newItemStack()
 				);
   }
 	
