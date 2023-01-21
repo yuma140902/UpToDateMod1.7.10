14e9a3c0e7514a1ae68fa1ef6cabace6af522ebb
yuma140902 <yuma140902@gmail.com>
2023-01-21 15:01:48 +0900

    update DownloadTaskStatus

--- src/main/java/yuma140902/uptodatemod/vrl/download/DownloadTaskStatus.java
+++ src/main/java/yuma140902/uptodatemod/vrl/download/DownloadTaskStatus.java
@@ -2,9 +2,8 @@ package yuma140902.uptodatemod.vrl.download;
 
 public enum DownloadTaskStatus {
     NOT_STARTED,
-    DOWNLOADING_FIRST_TIME,
-    RE_DOWNLOADING_BECAUSE_HASH_DOES_NOT_MATCH,
-    SKIPPED_BECAUSE_ALREADY_DOWNLOADED,
+    DOWNLOADING,
+    SKIPPED,
     DOWNLOADED,
     FAILED,
 }
