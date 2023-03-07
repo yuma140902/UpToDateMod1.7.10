6e35a48e1f356d78f024504b6fa716bfdcd02abb
yuma14 <yuma140902@gmail.com>
2023-03-08 04:53:16 +0900

    scala: MobLoot

--- src/main/scala/yuma140902/yumalib/api/util/ItemWithMetadata.scala
+++ src/main/scala/yuma140902/yumalib/api/util/ItemWithMetadata.scala
@@ -6,12 +6,12 @@ import net.minecraft.item.{Item, ItemStack}
 /**
  * アイテムとメタの組み合わせ。ItemStackだとcountやNBTが付いていて過剰な場合があるため作った
  */
-case class ItemWithMetadata(item: Item, meta: Int) {
+case class ItemWithMetadata(item: Item, meta: Int = 0) {
   def newItemStack: ItemStack = new ItemStack(item, 1, meta)
 }
 
 object ItemWithMetadata {
-  def fromBlock(block: Block, meta: Int): Option[ItemWithMetadata] = {
+  def fromBlock(block: Block, meta: Int = 0): Option[ItemWithMetadata] = {
     val item = Option(Item.getItemFromBlock(block));
     item.map(item => ItemWithMetadata(item, meta))
   }
