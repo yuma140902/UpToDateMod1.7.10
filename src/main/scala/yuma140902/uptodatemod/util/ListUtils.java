35b6989da1d22b63ae02d6140e4827f70c39547a
yuma14 <yuma140902@gmail.com>
2023-03-08 06:31:31 +0900

    deprecate ListUtils

--- src/main/scala/yuma140902/uptodatemod/util/ListUtils.java
+++ src/main/scala/yuma140902/uptodatemod/util/ListUtils.java
@@ -2,23 +2,28 @@ package yuma140902.uptodatemod.util;
 
 import java.util.List;
 
+/**
+ * @deprecated use scala instead
+ */
+@Deprecated
 public final class ListUtils {
-	private ListUtils() {}
-	
-	public static boolean contains(int[] list, int item) {
-		for(int listItem : list) {
-			if(listItem == item) return true; 
-		}
-		return false;
-	}
-	
-	//listにstrが含まれているかどうか
-	public static boolean contains(List<String> list, String str) {
-		for(String strInList : list) {
-			if(strInList.equals(str)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    private ListUtils() {
+    }
+
+    public static boolean contains(int[] list, int item) {
+        for (int listItem : list) {
+            if (listItem == item) return true;
+        }
+        return false;
+    }
+
+    //listにstrが含まれているかどうか
+    public static boolean contains(List<String> list, String str) {
+        for (String strInList : list) {
+            if (strInList.equals(str)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
